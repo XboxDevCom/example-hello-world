9e6b2e7b31216b51ff112a1e3633c781a8884962
Huskynarr <11574473+Huskynarr@users.noreply.github.com>
2026-08-15 16:16:12 +0200

    Xbox Standards: RequiresPointerMode, SafeArea, ElementSoundPlayer, UseSystemFocusVisuals, improved XAML & NuGet update

--- Hello World/MainPage.xaml.cs
+++ Hello World/MainPage.xaml.cs
@@ -1,25 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+﻿using Windows.UI.Xaml.Controls;
 
 namespace Hello_World
 {
-    /// <summary>
-    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
